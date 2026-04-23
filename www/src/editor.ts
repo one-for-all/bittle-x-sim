24db4dc11cf48c8f307a701458afa38f64517415
Jay Li <jn.sapiens@gmail.com>
2026-04-23 23:38:12 +0900

    fix: threejs view shrink properly

--- www/src/editor.ts
+++ www/src/editor.ts
@@ -74,10 +74,8 @@ document.getElementById("toggleSidebar")!.addEventListener("click", () => {
   window.dispatchEvent(new Event("resize"));
 });
 
-const sidebar = document.getElementById("sidebar")!;
 const mainResizeHandle = document.getElementById("mainResizeHandle")!;
 const container = document.querySelector(".container") as HTMLElement;
-const threejs = document.getElementById("threejs")!;
 
 mainResizeHandle.addEventListener("mousedown", (e) => {
   e.preventDefault();
@@ -86,9 +84,12 @@ mainResizeHandle.addEventListener("mousedown", (e) => {
 });
 
 function resizeMain(e: MouseEvent) {
-  const containerWidth = e.clientX - container.getBoundingClientRect().left;
-  if (containerWidth > 200) {
-    container.style.flex = `0 0 ${containerWidth}px`;
+  const containerRect = container.getBoundingClientRect();
+  const newWidth = e.clientX - containerRect.left;
+  const maxWidth = window.innerWidth * 0.8; // Prevent container from taking more than 80%
+
+  if (newWidth > 200 && newWidth < maxWidth) {
+    container.style.flex = `0 0 ${newWidth}px`;
   }
 
   window.dispatchEvent(new Event("resize"));
