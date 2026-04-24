92b47c1fea4a2aad616ed7bfa89d1687bd56663a
Jay Li <jn.sapiens@gmail.com>
2026-04-24 18:56:46 +0900

    refactor: rename container to editor-container

--- www/src/editor.ts
+++ www/src/editor.ts
@@ -75,7 +75,9 @@ document.getElementById("toggleSidebar")!.addEventListener("click", () => {
 });
 
 const mainResizeHandle = document.getElementById("mainResizeHandle")!;
-const container = document.querySelector(".container") as HTMLElement;
+const editorContainer = document.querySelector(
+  ".editor-container",
+) as HTMLElement;
 
 mainResizeHandle.addEventListener("mousedown", (e) => {
   e.preventDefault();
@@ -84,12 +86,12 @@ mainResizeHandle.addEventListener("mousedown", (e) => {
 });
 
 function resizeMain(e: MouseEvent) {
-  const containerRect = container.getBoundingClientRect();
+  const containerRect = editorContainer.getBoundingClientRect();
   const newWidth = e.clientX - containerRect.left;
   const maxWidth = window.innerWidth * 0.8; // Prevent container from taking more than 80%
 
   if (newWidth > 200 && newWidth < maxWidth) {
-    container.style.flex = `0 0 ${newWidth}px`;
+    editorContainer.style.flex = `0 0 ${newWidth}px`;
   }
 
   window.dispatchEvent(new Event("resize"));
