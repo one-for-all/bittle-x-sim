3ea9deef9c62d8b005097ddac373ee3ed2c9fdba
Jay Li <jn.sapiens@gmail.com>
2026-04-25 19:54:44 +0900

    feat: project dialog view

--- www/src/index.ts
+++ www/src/index.ts
@@ -13,8 +13,14 @@ initSimulator();
 openFile(Object.keys(files)[0]);
 
 document.getElementById("newProject")!.addEventListener("click", () => {
+  const dialog = document.getElementById("newProjectDialog")!;
+  dialog.classList.toggle("hidden");
+});
+
+document.getElementById("createProjectBtn")!.addEventListener("click", () => {
   resetFiles();
   renderExplorer();
   renderFileBar();
   openFile("main.ino");
+  document.getElementById("newProjectDialog")!.classList.add("hidden");
 });
