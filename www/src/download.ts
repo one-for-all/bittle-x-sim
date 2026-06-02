024c3cd0efa786f64210e80453d2540b180e2b5f
Jay Li <jn.sapiens@gmail.com>
2026-06-03 00:28:44 +0900

    feat: match download zip filename with ino filename

--- www/src/download.ts
+++ www/src/download.ts
@@ -1,22 +1,29 @@
-import JSZip from 'jszip';
-import { files } from './files';
+import JSZip from "jszip";
+import { files, inoFileName } from "./files";
 
 export function setupDownload() {
-    document.getElementById("downloadButton")!.addEventListener("click", async () => {
-        const zip = new JSZip();
+  document
+    .getElementById("downloadButton")!
+    .addEventListener("click", async () => {
+      const zip = new JSZip();
 
-        for (const [filename, fileEntry] of Object.entries(files)) {
-            zip.file(filename, fileEntry.content);
-        }
+      for (const [filename, fileEntry] of Object.entries(files)) {
+        zip.file(filename, fileEntry.content);
+      }
 
-        const content = await zip.generateAsync({ type: 'blob' });
-        const url = URL.createObjectURL(content);
-        const a = document.createElement('a');
-        a.href = url;
-        a.download = 'bittle_x_project.zip';
-        document.body.appendChild(a);
-        a.click();
-        document.body.removeChild(a);
-        URL.revokeObjectURL(url);
+      const content = await zip.generateAsync({ type: "blob" });
+      const url = URL.createObjectURL(content);
+      const a = document.createElement("a");
+
+      const inoName = inoFileName();
+      a.download = inoName
+        ? inoName.replace(/\.ino$/, ".zip")
+        : "bittle_x_project.zip";
+
+      document.body.appendChild(a);
+      a.href = url;
+      a.click();
+      document.body.removeChild(a);
+      URL.revokeObjectURL(url);
     });
 }
